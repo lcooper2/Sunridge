2d3719e769db833543b8f442f4e3ac850e67e545
Erika Fermin <erikafermin@mail.weber.edu>
2020-03-12 08:14:39 -0600

    adding database

---
+++ Sunridge/Data/Migrations/20200312141231_initalonmaster.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Sunridge.Data.Migrations
+{
+    public partial class initalonmaster : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
