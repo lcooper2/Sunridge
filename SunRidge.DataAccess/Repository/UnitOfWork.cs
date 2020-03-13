51e9a32f44247175a8d91def17398f7ac0b7b5c1
Erika Fermin <erikafermin@mail.weber.edu>
2020-03-13 15:12:41 -0600

    repo and unit ofwork

---
+++ SunRidge.DataAccess/Repository/UnitOfWork.cs
@@ -0,0 +1,27 @@
+﻿using Sunridge.DataAccess.Data.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sunridge.DataAccess.Data.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _db;
+        public UnitOfWork(ApplicationDbContext db)
+        {
+            _db = db;
+           
+        }
+
+        public void Dispose()
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+    }
+}
