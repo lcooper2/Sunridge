00dd8bd05090a2239773b6195ffeec017579b6e1
Taylor Hatch <taylorhatch2@gmail.com>
2020-03-16 13:46:11 -0600

    Added Repository and IRepository to the DataAccess folder also added the banner model to Models

---
+++ SunRidge.DataAccess/Data/Repository/IRepository/IRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Sunridge.DataAccess.Data.Repository.IRepository
+{
+   public interface IRepository<T> where T : class
+    {
+        // get by id
+        T Get(int id);
+
+        IEnumerable<T> GetAll(Expression<Func<T, bool>> filter = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+            string includeProperties = null);
+
+        T GetFirstOrDefault(
+            Expression<Func<T, bool>> filter = null,
+            string includeProperties = null);
+
+        void Add(T entity);
+
+        void Remove(int id);
+
+        void Remove(T entity);
+
+        public void RemoveListOfObjects(IEnumerable<T> entity);
+
+    }
+}
---
+++ SunRidge.Models/Banner.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Sunridge.Models
+{
+   public class Banner
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [Display(Name ="Header")]
+        public string Header { get; set; }
+        public string Body { get; set; }
+        public string Image { get; set; }
+    }
+}
