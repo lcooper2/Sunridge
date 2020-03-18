cfac08839f45fd7131807aa9358d12972338a327
Erika Fermin <erikafermin@mail.weber.edu>
2020-03-18 10:15:23 -0600

    App User Repo

---
+++ SunRidge.DataAccess/Repository/ApplicationUserRepository.cs
@@ -0,0 +1,24 @@
+﻿using Sunridge.DataAccess.Data;
+using Sunridge.DataAccess.Data.Repository;
+using Sunridge.DataAccess.Repository.IRepository;
+using Sunridge.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sunridge.DataAccess.Repository
+{
+    public class ApplicationUserRepository : Repository<ApplicationUser>, IApplicationUserRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public ApplicationUserRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(ApplicationUser applicationUser)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SunRidge.DataAccess/Repository/IRepository/ApplicationUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using Sunridge.DataAccess.Data.Repository.IRepository;
+using Sunridge.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sunridge.DataAccess.Repository.IRepository
+{
+    public interface IApplicationUserRepository : IRepository<ApplicationUser>
+    {
+        void Update(ApplicationUser applicationUser);
+    }
+}
