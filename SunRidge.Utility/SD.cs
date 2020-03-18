24b6a08099abb47af2f95478f5073e7614b5bb37
Taylor Hatch <taylorhatch2@gmail.com>
2020-03-18 15:57:20 -0600

    update to SD

--- SunRidge.Utility/SD.cs
+++ SunRidge.Utility/SD.cs
@@ -6,8 +6,8 @@ namespace Sunridge.Utility
 {
     public class SD
     {
-        public const string OwnerRole = "Owner";
-        public const string AdminRole = "Board Member";
+        //public const string OwnerRole = "Owner";
+        //public const string AdminRole = "Board Member";
 
         public const string StatusActive = "Active";
         public const string StatusInactive = "Inactive";
