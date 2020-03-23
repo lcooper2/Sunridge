e7767237354158730d573b0e63eda8be69702597
Derek <book911@gmail.com>
2020-03-23 17:18:36 -0600

    Added previous site's css, homepage, images, and added user roles to registration

--- Sunridge/Pages/Home/Index.cshtml.cs
+++ Sunridge/Pages/Home/Index.cshtml.cs
@@ -4,14 +4,25 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Sunridge.DataAccess.Data.Repository.IRepository;
+using Sunridge.Models;
 
 namespace Sunridge.Pages.Home
 {
     public class IndexModel : PageModel
     {
+        //private readonly IUnitOfWork _unitOfWork;
+
+        //public IndexModel(IUnitOfWork unitOfWork)
+        //{
+        //    _unitOfWork = unitOfWork;
+        //}
+
+        [BindProperty]
+        public List<Banner> BannerList { get; set; }
         public void OnGet()
         {
-
+            //BannerList = _unitOfWork.Banner.GetAll().ToList();
         }
     }
 }
