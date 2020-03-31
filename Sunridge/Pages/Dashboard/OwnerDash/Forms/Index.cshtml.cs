79b0747bb86e56ab19d81c55bfb847818113bef8
Aurabux <erikafermin@mail.weber.edu>
2020-03-31 14:14:07 -0600

    form page made but function is having design problems

--- Sunridge/Pages/Dashboard/OwnerDash/Forms/Index.cshtml.cs
+++ Sunridge/Pages/Dashboard/OwnerDash/Forms/Index.cshtml.cs
@@ -5,9 +5,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace Sunridge
+namespace Sunridge.Pages.Dashboard.OwnerDash.Forms
 {
-    public class FormsModel : PageModel
+    public class IndexModel : PageModel
     {
         public void OnGet()
         {
---
+++ Sunridge/Pages/Dashboard/OwnerDash/Forms/SC.cshtml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Sunridge.DataAccess.Data.Repository.IRepository;
+
+namespace Sunridge.Pages.Dashboard.OwnerDash.Forms
+{
+    public class SCModel : PageModel
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public SCModel(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+
+        }
+        [BindProperty]
+        public Models.FormResponse FormResObj { get; set; }
+        public IActionResult OnGet(int? id)
+        {
+            FormResObj = new Models.FormResponse();
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+            if (claim != null)
+            {
+               
+                if (id != null)
+                {
+                    FormResObj = _unitOfWork.FormResponse.GetFirstOrDefault(u => u.Id == id);
+                    FormResObj.FormType = "SC";
+                    FormResObj.SubmitDate = DateTime.Now;
+                    FormResObj.ApplicationUserId = claim.Value;
+                    if (FormResObj == null)
+                    {
+                        return NotFound();
+                    }
+                }  
+            }
+            return Page();
+        }
+        
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            if (FormResObj.Id == 0)
+            {
+                _unitOfWork.FormResponse.Add(FormResObj);
+            }
+            else
+            {
+                _unitOfWork.FormResponse.Update(FormResObj);
+            }
+            _unitOfWork.Save();
+            return RedirectToPage("./Index");
+        }
+    }
+}
