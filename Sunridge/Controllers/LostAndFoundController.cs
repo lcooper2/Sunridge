0a08ffb6803c8f70f0c50537056c138b8fb67298
Taylor Hatch <taylorhatch2@gmail.com>
2020-04-02 15:59:05 -0600

    updated Lost and found, Admin can now add multiple photos to a Lost and found object

--- Sunridge/Controllers/LostAndFoundController.cs
+++ Sunridge/Controllers/LostAndFoundController.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Sunridge.DataAccess.Data.Repository.IRepository;
@@ -13,10 +15,12 @@ namespace Sunridge.Controllers
     public class LostAndFoundController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
+        private readonly IWebHostEnvironment _hostingEnvironment;
 
-        public LostAndFoundController(IUnitOfWork unitOfWork)
+        public LostAndFoundController(IUnitOfWork unitOfWork, IWebHostEnvironment hostingEnvironment)
         {
             _unitOfWork = unitOfWork;
+            _hostingEnvironment = hostingEnvironment;
         }
 
         public IActionResult Get()
@@ -26,15 +30,38 @@ namespace Sunridge.Controllers
         [HttpDelete("{id}")]
         public IActionResult Delete(int id)
         {
-            var objFromDb = _unitOfWork.LostAndFound.GetFirstOrDefault(u => u.Id == id);
-            if (objFromDb == null)
+            try
+            {
+                var ObjFromDb = _unitOfWork.LostAndFound.GetFirstOrDefault(u => u.Id == id);
+                if (ObjFromDb == null)
+                {
+                    return Json(new { success = false, message = "Error while deleting" });
+                }
+
+                var images = _unitOfWork.LostAndFoundImage.GetAll(x => x.LostAndFoundId == ObjFromDb.Id).ToList();
+                // physically Remove any uploaded images
+                
+                foreach (var image in images)
+                {
+                    var imagePath = Path.Combine(_hostingEnvironment.WebRootPath, image.ImageURL.TrimStart('\\'));
+
+
+                    if (System.IO.File.Exists(imagePath))
+                    {
+                        System.IO.File.Delete(imagePath);
+                    }
+                }
+                _unitOfWork.LostAndFound.Remove(ObjFromDb);
+
+                _unitOfWork.Save();
+            }
+            catch (Exception)
             {
                 return Json(new { success = false, message = "Error while deleting" });
             }
-            _unitOfWork.LostAndFound.Remove(objFromDb);
-            _unitOfWork.Save();
+
             return Json(new { success = true, message = "Deletion was Successful" });
         }
-    
- }
+    }
+
 }
