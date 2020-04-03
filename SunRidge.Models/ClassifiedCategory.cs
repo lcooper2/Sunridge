b4f153a2daa0140924336b3650fe2c6f23d33e13
Aurabux <erikafermin@mail.weber.edu>
2020-04-03 10:15:53 -0600

    database erros, yikes

--- SunRidge.Models/ClassifiedCategory.cs
+++ SunRidge.Models/ClassifiedCategory.cs
@@ -7,6 +7,7 @@ namespace Sunridge.Models
 {
     public class ClassifiedCategory
     {
+        [Key]
         public int Id { get; set; }
         [Display(Name = "Category")]
         public string Description { get; set; }
