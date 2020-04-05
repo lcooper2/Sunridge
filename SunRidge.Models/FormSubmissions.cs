7c08cb44263c8c83328cbecf3277015e8df86649
Aurabux <erikafermin@mail.weber.edu>
2020-04-05 15:31:46 -0600

    adding a new model for form rework

---
+++ SunRidge.Models/FormSubmissions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Sunridge.Models
+{
+    public class FormSubmissions
+    {
+        [Key]
+        public int Id { get; set; }
+
+        /*To port info of person who is filling */
+        [Display(Name = "FormId")]
+        public string FormId { get; set; }
+
+        [NotMapped]
+        [ForeignKey("ClaimLossId")]
+        public virtual ClaimLoss ClaimLoss { get; set; }
+    }
+}
--- Sunridge/Pages/Dashboard/AdminDash/Forms/FormRespnsepage.cshtml.cs
+++ Sunridge/Pages/Dashboard/AdminDash/Forms/FormRespnsepage.cshtml.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Sunridge.DataAccess.Data.Repository.IRepository;
-using Sunridge.Models;
+using Sunridge.Models.ViewModels;
 
 namespace Sunridge.Pages.Dashboard.AdminDash.Forms
 {
@@ -13,15 +13,51 @@ namespace Sunridge.Pages.Dashboard.AdminDash.Forms
     {
         private readonly IUnitOfWork _unitOfWork;
 
-        public IEnumerable<FormResponse> FormList { get; set; }
+        //public IEnumerable<FormResponse> FormList { get; set; }
 
         public FormRespnsepageModel(IUnitOfWork unitOfWork)
         {
             _unitOfWork = unitOfWork;
         }
-        public void OnGet()
+        [BindProperty]
+        public FormResponseVM FormResObj { get; set; }
+       
+            //FormList = _unitOfWork.FormResponse.GetAll(null, q => q.OrderBy(c => c.Resolved), null);
+        public IActionResult OnGet(int? id)
         {
-            FormList = _unitOfWork.FormResponse.GetAll(null, q => q.OrderBy(c => c.Resolved), null);
+            FormResObj = new FormResponseVM
+            {
+               // WIKList = _unitOfWork.InKindWorkHours.GetWikListForDropDown(),
+                
+
+                FormResponse = new Models.FormResponse()
+            };
+            if (id != null)
+            {
+                FormResObj.FormResponse = _unitOfWork.FormResponse.GetFirstOrDefault(u => u.Id == id);
+                if (FormResObj == null)
+                {
+                    return NotFound();
+                }
+            }
+            return Page();
+        }
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            if (FormResObj.FormResponse.Id == 0)
+            {
+                _unitOfWork.FormResponse.Add(FormResObj.FormResponse);
+            }
+            else
+            {
+                _unitOfWork.FormResponse.Update(FormResObj.FormResponse);
+            }
+            _unitOfWork.Save();
+            return RedirectToPage("./FormRespnsepage");
         }
     }
 }
