f752a21ca2fa4fb4ca418de23c9e5c996b621b87
Derek <book911@gmail.com>
2020-04-11 11:34:26 -0600

    Added NewsItem model

---
+++ SunRidge.DataAccess/Data/Repository/IRepository/INewsItemRepository.cs
@@ -0,0 +1,13 @@
+﻿using Sunridge.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sunridge.DataAccess.Data.Repository.IRepository
+{
+    public interface INewsItemRepository : IRepository<NewsItem>
+    {
+        public void Update(NewsItem newsItem);
+
+    }
+}
