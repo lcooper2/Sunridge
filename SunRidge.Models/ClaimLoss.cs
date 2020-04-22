bee99de859fa8ce3c38b6c712c1bb9f2e2ce87a4
Aurabux <erikafermin@mail.weber.edu>
2020-04-22 11:53:21 -0600

    date time fixes

--- SunRidge.Models/ClaimLoss.cs
+++ SunRidge.Models/ClaimLoss.cs
@@ -16,7 +16,8 @@ namespace Sunridge.Models
 
         //public string? AttorneyInfo { get; set; }
         public string Type { get; set; }
-
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy-MM-dd}")]
         public DateTime DateofIncident { get; set; }
 
         /*To port info of person who is filling */
